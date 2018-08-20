a48e6920693bdfec90dfd1309b7984b3149c7a4a
Snow <throwawayyay11@gmail.com>
2018-08-19 22:31:58 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`-----------------------------------\nOofbotV2 has launched!\nRunning in ${client.guilds.size} guilds with ${client.users.size} users!`);
+});
+
+
+
+
+client.login('NDc1MDM4NzgwOTU3MzkyOTEx.Dlu_LQ.OnN4vu9xVHiOYFZYCK0uBo5ECP0');
